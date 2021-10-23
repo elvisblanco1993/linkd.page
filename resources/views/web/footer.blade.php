e8eae15eef143be86a8c127047b00d72983e64f5
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2021-10-23 14:54:24 -0400

    Privacy policy and terms of service now live

--- resources/views/web/footer.blade.php
+++ resources/views/web/footer.blade.php
@@ -5,9 +5,9 @@
                 <span class="text-indigo-600">linkd</span> is a project by <a href="https://twitter.com/_ebglez">Elvis Blanco</a>
             </div>
             <div class="flex items-center gap-4">
-                <a href="" class="text-sm font-light hover:text-indigo-600 transition">Support</a>
-                <a href="" class="text-sm font-light hover:text-indigo-600 transition">Privacy</a>
-                <a href="" class="text-sm font-light hover:text-indigo-600 transition">Terms</a>
+                <a href="https://linkd.tawk.help" target="_blank" class="text-sm font-light hover:text-indigo-600 transition">Support</a>
+                <a href="/privacy-policy" class="text-sm font-light hover:text-indigo-600 transition">Privacy</a>
+                <a href="/terms-of-service" class="text-sm font-light hover:text-indigo-600 transition">Terms</a>
             </div>
         </div>
     </div>
