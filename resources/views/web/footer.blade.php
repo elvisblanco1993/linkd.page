f7f4677a579bb13ee0f0ab2b314024f63d29b9f5
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2021-10-26 22:45:54 -0400

    updated support link

--- resources/views/web/footer.blade.php
+++ resources/views/web/footer.blade.php
@@ -5,7 +5,7 @@
                 <span class="text-indigo-600">linkd</span> is a project by <a href="https://twitter.com/_ebglez">Elvis Blanco</a>
             </div>
             <div class="flex items-center gap-4">
-                <a href="https://linkd.tawk.help" target="_blank" class="text-sm font-light hover:text-indigo-600 transition">Support</a>
+                <a href="mailto:support@linkd.page" target="_blank" class="text-sm font-light hover:text-indigo-600 transition">Support</a>
                 <a href="/privacy-policy" class="text-sm font-light hover:text-indigo-600 transition">Privacy</a>
                 <a href="/terms-of-service" class="text-sm font-light hover:text-indigo-600 transition">Terms</a>
             </div>
